b6a05dfb68ceab1800697d34c6bffa61b3896130
sgc93 <smachewgedefaw@gmail.com>
2025-11-26 23:05:12 +0300

    fix: follow Joi schema to add custom message

--- src/modules/users/user.validator.ts
+++ src/modules/users/user.validator.ts
@@ -1,7 +1,10 @@
 import Joi from "joi";
 
 export const createUserSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().min(6, 'Password should be atleast 6 characters').required()
-})
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string()
+    .min(6)
+    .required()
+    .messages({ "string.min": "Password should be atleast 6 characters" })
+});
