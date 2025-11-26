de2996b1f6df0f18b923e1bb8682fc878d7a3b6c
sgc93 <smachewgedefaw@gmail.com>
2025-11-26 23:57:52 +0300

    update: remove unused schema defination

--- src/modules/users/user.validator.ts
+++ src/modules/users/user.validator.ts
@@ -1,10 +1,5 @@
 import Joi from "joi";
 
-export const createUserSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  password: Joi.string()
-    .min(6)
-    .required()
-    .messages({ "string.min": "Password should be atleast 6 characters" })
+export const updateUserSchema = Joi.object({
+  name: Joi.string().required()
 });
