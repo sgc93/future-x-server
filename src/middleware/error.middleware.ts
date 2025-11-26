57823a7cc1049f56bc4e1bf68ee14b7696ec477f
sgc93 <smachewgedefaw@gmail.com>
2025-11-26 04:10:31 +0300

    create: middlewares to handle auth

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,9 @@
+// src/middlewares/error.middleware.ts
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  console.error(err);
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ message });
+}
