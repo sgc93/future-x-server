18b8b1aee0ce3f54c31ddfa10fb0bcdb61524079
sgc93 <smachewgedefaw@gmail.com>
2025-11-26 04:10:01 +0300

    update: add user model

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import config from "./config";
+import User from "../modules/users/user.model";
 
 const sequelize: Sequelize = new Sequelize({
   host: config.db.host,
@@ -9,6 +10,7 @@ const sequelize: Sequelize = new Sequelize({
   username: config.db.username!,
   password: config.db.password!,
   logging: false,
+  models: [User]
 });
 
 export default sequelize;
